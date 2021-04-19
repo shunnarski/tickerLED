538e12d5361eb2dc8b643fec5b0c155f9027a6ff
shunnarski <ashunnarah9597@gmail.com>
2021-04-19 01:01:41 -0500

    Adding some skeleton code and getting current bitcoin price

---
+++ src/interpolate_data.py
@@ -0,0 +1,18 @@
+import pandas
+import numpy
+import scipy
+
+"""
+Gets the interpolated data results of the raw stock/crypto values to construct the graph
+based on the number of LED lights available in the grid being used to produce the graph.
+
+Params:
+
+raw_df - The raw values of the stock/crypto values
+led_grid_width - The width of the grid based in LED lights available
+led_grid_height - The height of the grid based on the LED lights available
+
+"""
+def get_interpolated_data_results(raw_df, led_grid_width, led_grid_height):
+    pass
+
